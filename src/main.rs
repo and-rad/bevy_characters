85d5d97df1a788a283e60f567a8333f28e076aa2
Andreas Raddau <and.rad@posteo.de>
2023-11-06 09:48:47 +0100

    Added camera to the main scene

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct MainCamera;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, camera_startup_system)
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
+fn camera_startup_system(mut cmd: Commands) {
+    cmd.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        MainCamera,
+    ));
+}
